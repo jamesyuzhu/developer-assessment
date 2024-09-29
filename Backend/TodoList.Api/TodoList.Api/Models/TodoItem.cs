3d4313848b289490c0d6528ce7d8a44fba6651a1
jamesyuzhu <james.zhu.office@gmail.com>
2024-09-29 13:08:11 +1300

    Create Models folder to test the new git account working

---
+++ Backend/TodoList.Api/TodoList.Api/Models/TodoItem.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace TodoList.Api.Models
+
+{
+    public class TodoItem
+    {
+        public Guid Id { get; set; }
+
+        public string Description { get; set; }
+
+        public bool IsCompleted { get; set; }
+    }
+}
