b1e1c763c80e7f1d97127519d13c536816430bf4
jamesyuzhu <james.zhu.office@gmail.com>
2024-10-04 14:39:42 +1300

    Apply two components to the App page and remove the redundant codes

--- Frontend-React/src/App.js
+++ Frontend-React/src/App.js
@@ -1,114 +1,42 @@
-import './App.css'
-import { Image, Alert, Button, Container, Row, Col, Form, Table, Stack } from 'react-bootstrap'
-import React, { useState, useEffect } from 'react'
+import './App.css';
+import { Image, Alert, Container, Row, Col } from 'react-bootstrap';
+import React, { useState, useEffect } from 'react';
+import AddTodoItemForm from './components/AddTodoItemForm';
+import TodoItemList from './components/TodoItemList';
 
 const axios = require('axios')
 
 const App = () => {
-  const [description, setDescription] = useState('')
-  const [items, setItems] = useState([])
+  const [items, setItems] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
 
-  useEffect(() => {
-    // todo
+  const fetchItems = async () => {
+    try {
+      const response = await fetch('https://localhost:44397/api/todoitems');
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      const result = await response.json();
+      setItems(result);
+      setLoading(false); 
+      setError('');
+    } catch (error) {
+      setError(error.message);
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {    
+    fetchItems();     
   }, [])
 
-  const renderAddTodoItemContent = () => {
-    return (
-      <Container>
-        <h1>Add Item</h1>
-        <Form.Group as={Row} className="mb-3" controlId="formAddTodoItem">
-          <Form.Label column sm="2">
-            Description
-          </Form.Label>
-          <Col md="6">
-            <Form.Control
-              type="text"
-              placeholder="Enter description..."
-              value={description}
-              onChange={handleDescriptionChange}
-            />
-          </Col>
-        </Form.Group>
-        <Form.Group as={Row} className="mb-3 offset-md-2" controlId="formAddTodoItem">
-          <Stack direction="horizontal" gap={2}>
-            <Button variant="primary" onClick={() => handleAdd()}>
-              Add Item
-            </Button>
-            <Button variant="secondary" onClick={() => handleClear()}>
-              Clear
-            </Button>
-          </Stack>
-        </Form.Group>
-      </Container>
-    )
+  async function handleAddCompleted() {
+    await fetchItems();
   }
 
-  const renderTodoItemsContent = () => {
-    return (
-      <>
-        <h1>
-          Showing {items.length} Item(s){' '}
-          <Button variant="primary" className="pull-right" onClick={() => getItems()}>
-            Refresh
-          </Button>
-        </h1>
-
-        <Table striped bordered hover>
-          <thead>
-            <tr>
-              <th>Id</th>
-              <th>Description</th>
-              <th>Action</th>
-            </tr>
-          </thead>
-          <tbody>
-            {items.map((item) => (
-              <tr key={item.id}>
-                <td>{item.id}</td>
-                <td>{item.description}</td>
-                <td>
-                  <Button variant="warning" size="sm" onClick={() => handleMarkAsComplete(item)}>
-                    Mark as completed
-                  </Button>
-                </td>
-              </tr>
-            ))}
-          </tbody>
-        </Table>
-      </>
-    )
-  }
-
-  const handleDescriptionChange = (event) => {
-    // todo
-  }
-
-  async function getItems() {
-    try {
-      alert('todo')
-    } catch (error) {
-      console.error(error)
-    }
-  }
-
-  async function handleAdd() {
-    try {
-      alert('todo')
-    } catch (error) {
-      console.error(error)
-    }
-  }
-
-  function handleClear() {
-    setDescription('')
-  }
-
-  async function handleMarkAsComplete(item) {
-    try {
-      alert('todo')
-    } catch (error) {
-      console.error(error)
-    }
+  function handleError(error) {
+    setError(error);
   }
 
   return (
@@ -143,11 +71,13 @@ const App = () => {
           </Col>
         </Row>
         <Row>
-          <Col>{renderAddTodoItemContent()}</Col>
-        </Row>
-        <br />
+          <Col><AddTodoItemForm onSave={handleAddCompleted} onError={handleError}></AddTodoItemForm></Col>
+        </Row>        
         <Row>
-          <Col>{renderTodoItemsContent()}</Col>
+          <Col>{error && <div style={{ color: 'red' }}>Error: {error}</div>}</Col>
+        </Row>
+        <Row>
+          <Col><TodoItemList todoItems={items} onRefresh={fetchItems} onSave={fetchItems} onError={handleError}></TodoItemList></Col>
         </Row>
       </Container>
       <footer className="page-footer font-small teal pt-4">
